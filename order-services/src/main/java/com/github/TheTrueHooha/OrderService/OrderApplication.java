e75c09dfa335ba879a9381b8b4b43975019e6cb2
David Ogar <owogogahhero@outlook.com>
2022-05-26 07:57:38 +0100

    refactor: added bean configuration + rest template

--- order-services/src/main/java/com/github/TheTrueHooha/OrderService/OrderApplication.java
+++ order-services/src/main/java/com/github/TheTrueHooha/OrderService/OrderApplication.java
@@ -2,10 +2,18 @@ package com.github.TheTrueHooha.OrderService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class OrderApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 }
