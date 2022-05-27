76fd9e696ebf1087cca6efc42bf38876db535ebd
David Ogar <owogogahhero@outlook.com>
2022-05-27 09:45:44 +0100

    refactor: added load balanced annotations

--- order-services/src/main/java/com/github/TheTrueHooha/OrderService/OrderApplication.java
+++ order-services/src/main/java/com/github/TheTrueHooha/OrderService/OrderApplication.java
@@ -2,10 +2,13 @@ package com.github.TheTrueHooha.OrderService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderApplication {
 
     public static void main(String[] args) {
@@ -13,6 +16,7 @@ public class OrderApplication {
     }
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
