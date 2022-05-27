a9be89349195c39ae0425ad30e2a1d26af3a9a27
David Ogar <owogogahhero@outlook.com>
2022-05-27 09:50:29 +0100

    refactor: added load balanced annotation

--- payments-services/src/main/java/com/github/TheTrueHooha/PaymentService/PaymentApplication.java
+++ payments-services/src/main/java/com/github/TheTrueHooha/PaymentService/PaymentApplication.java
@@ -2,9 +2,12 @@ package com.github.TheTrueHooha.PaymentService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(PaymentApplication.class, args);
     }
