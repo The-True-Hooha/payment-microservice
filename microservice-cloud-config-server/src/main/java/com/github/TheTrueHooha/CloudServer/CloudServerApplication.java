d61b41f39c01439e76610e5e438d6448d33b00b2
David Ogar <owogogahhero@outlook.com>
2022-05-28 22:28:43 +0100

    added cloud serve configurationsr

---
+++ microservice-cloud-config-server/src/main/java/com/github/TheTrueHooha/CloudServer/CloudServerApplication.java
@@ -0,0 +1,16 @@
+package com.github.TheTrueHooha.CloudServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class CloudServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudServerApplication.class, args);
+    }
+}
